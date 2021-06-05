f3ccdba45462cf039ff1f12449dbc4feaac62a65
grzi <jeremy.thulliez@gmail.com>
2021-06-05 18:49:35 +0200

    fix: forgotten clean of Children component
    FIXES #121

--- src/core/components/maths/hierarchy.rs
+++ src/core/components/maths/hierarchy.rs
@@ -7,4 +7,5 @@ pub struct Parent(pub Entity);
 /// A component creating a link to the wrapped entities
 /// This component will be automatically added to an entity by Scion
 /// if a component references this entity with a [`Parent`] component
+#[derive(Debug)]
 pub struct Children(pub Vec<Entity>);
--- src/core/systems/hierarchy_system.rs
+++ src/core/systems/hierarchy_system.rs
@@ -1,13 +1,11 @@
-use legion::{
-    system,
-    systems::CommandBuffer,
-    world::{EntityAccessError, SubWorld},
-    Entity, Query,
-};
+use legion::{system, systems::CommandBuffer, world::{EntityAccessError, SubWorld}, Entity, Query, EntityStore};
 
 use crate::core::components::maths::hierarchy::{Children, Parent};
+use futures::StreamExt;
 
 /// System responsible to add/modify Children components to the entities referenced by a Parent component
+/// If the parent component referenced by the Children one is not found, then it deletes the entity
+/// If an entity has a Children component referencing non existing children, then this system will remove these references
 #[system]
 pub(crate) fn children_manager(
     world: &mut SubWorld,
@@ -39,6 +37,21 @@ pub(crate) fn children_manager(
             }
         }
     });
+
+    query_children.for_each_mut(&mut w2, |(e, p)|{
+        if let Some(mut p) = p {
+            let mut to_delete = Vec::new();
+            for (index, child) in p.0.iter().enumerate(){
+                if w1.entry_ref(*child).is_err(){
+                    to_delete.push(index);
+                }
+            }
+            to_delete.reverse();
+            to_delete.iter().for_each(|index|{
+                p.0.remove(*index);
+            });
+        }
+    });
 }
 
 #[cfg(test)]
@@ -49,7 +62,7 @@ mod tests {
     use crate::core::components::maths::hierarchy::{Children, Parent};
 
     #[test]
-    fn children_manager_system_test() {
+    fn children_manager_system_test_children_delete() {
         let mut world = World::default();
         let mut resources = Resources::default();
         let mut schedule = Schedule::builder()
@@ -71,5 +84,25 @@ mod tests {
         world.remove(parent);
         schedule.execute(&mut world, &mut resources);
         assert_eq!(true, world.entry(child).is_none());
+    }
+
+    #[test]
+    fn children_manager_system_test_parent_clean() {
+        let mut world = World::default();
+        let mut resources = Resources::default();
+        let mut schedule = Schedule::builder()
+            .add_system(children_manager_system())
+            .build();
+
+        let child = world.push((1,));
+        let parent = world.push((2,Children(vec![child])));
+
+        // We check that we have the child
+        assert_eq!(true,world.entry(parent).unwrap().get_component::<Children>().unwrap().0.contains(&child));
+        world.remove(child);
+
+        // we delete the child and then we execute the schedule and test that we have the good result
+        schedule.execute(&mut world, &mut resources);
+        assert_eq!(0, world.entry(parent).unwrap().get_component::<Children>().unwrap().0.len());
     }
 }
