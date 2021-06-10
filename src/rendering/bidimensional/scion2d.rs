04ffc64b33b2a92a1bf68720b19a7bcb197e7fc3
grzi <jeremy.thulliez@gmail.com>
2021-06-10 23:33:06 +0200

    feat: Optimize buffer creation and add a buffer cleaner
    FIXES #120

--- src/rendering/bidimensional/scion2d.rs
+++ src/rendering/bidimensional/scion2d.rs
@@ -77,6 +77,7 @@ impl ScionRenderer for Scion2D {
         } else {
             log::warn!("No camera has been found in resources");
         }
+        self.clean_buffers(world);
     }
 
     fn render(
@@ -108,179 +109,6 @@ impl ScionRenderer for Scion2D {
                 self.render_component(&frame, encoder, info);
             }
         }
-    }
-}
-
-fn load_texture_to_queue(
-    texture: &Texture,
-    queue: &mut Queue,
-    device: &Device,
-) -> (BindGroupLayout, BindGroup) {
-    let texture_size = wgpu::Extent3d {
-        width: texture.width as u32,
-        height: texture.height as u32,
-        depth_or_array_layers: 1,
-    };
-
-    let diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
-        size: texture_size,
-        mip_level_count: 1,
-        sample_count: 1,
-        dimension: wgpu::TextureDimension::D2,
-        format: wgpu::TextureFormat::Rgba8UnormSrgb,
-        usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
-        label: Some("diffuse_texture"),
-    });
-    queue.write_texture(
-        wgpu::ImageCopyTexture {
-            texture: &diffuse_texture,
-            mip_level: 0,
-            origin: wgpu::Origin3d::ZERO,
-        },
-        &*texture.bytes,
-        wgpu::ImageDataLayout {
-            offset: 0,
-            bytes_per_row: std::num::NonZeroU32::new((4 * texture.width) as u32),
-            rows_per_image: std::num::NonZeroU32::new(texture.height as u32),
-        },
-        texture_size,
-    );
-    let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
-    let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-        address_mode_u: wgpu::AddressMode::ClampToEdge,
-        address_mode_v: wgpu::AddressMode::ClampToEdge,
-        address_mode_w: wgpu::AddressMode::ClampToEdge,
-        mag_filter: wgpu::FilterMode::Nearest,
-        min_filter: wgpu::FilterMode::Nearest,
-        mipmap_filter: wgpu::FilterMode::Nearest,
-        ..Default::default()
-    });
-    let texture_bind_group_layout =
-        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            entries: &[
-                wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStage::FRAGMENT,
-                    ty: wgpu::BindingType::Texture {
-                        multisampled: false,
-                        view_dimension: wgpu::TextureViewDimension::D2,
-                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
-                    },
-                    count: None,
-                },
-                wgpu::BindGroupLayoutEntry {
-                    binding: 1,
-                    visibility: wgpu::ShaderStage::FRAGMENT,
-                    ty: wgpu::BindingType::Sampler {
-                        comparison: false,
-                        filtering: true,
-                    },
-                    count: None,
-                },
-            ],
-            label: Some("texture_bind_group_layout"),
-        });
-
-    let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-        layout: &texture_bind_group_layout,
-        entries: &[
-            wgpu::BindGroupEntry {
-                binding: 0,
-                resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
-            },
-            wgpu::BindGroupEntry {
-                binding: 1,
-                resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
-            },
-        ],
-        label: Some("diffuse_bind_group"),
-    });
-    (texture_bind_group_layout, diffuse_bind_group)
-}
-
-fn create_transform_uniform_bind_group(
-    device: &Device,
-    transform: &Transform,
-    camera: (&Camera, &Transform),
-    is_ui_component: bool,
-) -> (GlUniform, Buffer, BindGroupLayout, BindGroup) {
-    let uniform = GlUniform::from(UniformData {
-        transform,
-        camera,
-        is_ui_component,
-    });
-    let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("Uniform Buffer"),
-        contents: bytemuck::cast_slice(&[uniform]),
-        usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
-    });
-
-    let uniform_bind_group_layout =
-        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            entries: &[wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStage::VERTEX,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Uniform,
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            }],
-            label: Some("uniform_bind_group_layout"),
-        });
-
-    let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-        layout: &uniform_bind_group_layout,
-        entries: &[wgpu::BindGroupEntry {
-            binding: 0,
-            resource: uniform_buffer.as_entire_binding(),
-        }],
-        label: Some("uniform_bind_group"),
-    });
-
-    (
-        uniform,
-        uniform_buffer,
-        uniform_bind_group_layout,
-        uniform_bind_group,
-    )
-}
-
-fn get_default_color_attachment<'a>(frame: &'a SwapChainTexture, config: &'a ScionConfig,) -> RenderPassColorAttachment<'a> {
-    RenderPassColorAttachment {
-        view: &frame.view,
-        resolve_target: None,
-        ops: wgpu::Operations {
-            load: wgpu::LoadOp::Clear(
-                if let Some(color) = &config.window_config.as_ref().expect("Window config is missing").default_background_color {
-                    wgpu::Color {
-                        r: (color.red() as f32 / 255.) as f64,
-                        g: (color.green() as f32 / 255.) as f64,
-                        b: (color.blue() as f32 / 255.) as f64,
-                        a: color.alpha()  as f64,
-                    }
-                }else{
-                    wgpu::Color {
-                        r: 1.,
-                        g: 0.,
-                        b: 0.,
-                        a: 1.0,
-                    }
-                }),
-            store: true,
-        },
-    }
-}
-
-fn get_no_color_attachment(frame: &SwapChainTexture) -> RenderPassColorAttachment {
-    RenderPassColorAttachment {
-        view: &frame.view,
-        resolve_target: None,
-        ops: wgpu::Operations {
-            load: wgpu::LoadOp::Load,
-            store: true,
-        },
     }
 }
 
@@ -466,15 +294,24 @@ impl Scion2D {
         render_infos
     }
 
-    fn update_transforms(&mut self, main_world: &mut World, device: &&Device, queue: &mut Queue) {
+    fn update_transforms(&mut self, world: &mut World, device: &&Device, queue: &mut Queue) {
+        self.update_transforms_for_type::<Triangle>(world, &device, queue);
+        self.update_transforms_for_type::<Square>(world, &device, queue);
+        self.update_transforms_for_type::<Rectangle>(world, &device, queue);
+        self.update_transforms_for_type::<Sprite>(world,  &device, queue);
+        self.update_transforms_for_type::<UiImage>(world,  &device, queue);
+        self.update_transforms_for_type::<UiTextImage>(world,  &device, queue);
+    }
+
+    fn update_transforms_for_type<T: Component + Renderable2D>(&mut self, main_world: &mut World, device: &&Device, queue: &mut Queue) {
         let mut camera_query = <(&Camera, &Transform)>::query();
         let (camera_world, mut world) = main_world.split_for_query(&camera_query);
         let camera = camera_query
             .iter(&camera_world)
             .next()
             .expect("No camera has been found in the world after the security check");
-        for (entity, transform, optional_ui_component) in
-            <(Entity, &Transform, Option<&UiComponent>)>::query().iter_mut(&mut world)
+        for (entity, transform, optional_ui_component, _) in
+            <(Entity, &Transform, Option<&UiComponent>, &T)>::query().iter_mut(&mut world)
         {
             if !self.transform_uniform_bind_groups.contains_key(entity) {
                 let (uniform, uniform_buffer, glayout, group) = create_transform_uniform_bind_group(
@@ -500,7 +337,6 @@ impl Scion2D {
             }
         }
     }
-
     /// Loads in the queue materials that are not yet loaded.
     fn update_diffuse_bind_groups(
         &mut self,
@@ -543,6 +379,188 @@ impl Scion2D {
                 }
             }
         });
+    }
+
+    fn clean_buffers(&mut self, world: &mut World) {
+        let entities: Vec<&Entity> = <Entity>::query().iter(world).collect();
+        self.vertex_buffers.retain(|&k, _| entities.contains(&&k));
+        self.index_buffers.retain(|&k, _| entities.contains(&&k));
+        self.transform_uniform_bind_groups.retain(|&k, _| entities.contains(&&k));
+    }
+
+}
+
+
+fn load_texture_to_queue(
+    texture: &Texture,
+    queue: &mut Queue,
+    device: &Device,
+) -> (BindGroupLayout, BindGroup) {
+    let texture_size = wgpu::Extent3d {
+        width: texture.width as u32,
+        height: texture.height as u32,
+        depth_or_array_layers: 1,
+    };
+
+    let diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
+        size: texture_size,
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Rgba8UnormSrgb,
+        usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+        label: Some("diffuse_texture"),
+    });
+    queue.write_texture(
+        wgpu::ImageCopyTexture {
+            texture: &diffuse_texture,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+        },
+        &*texture.bytes,
+        wgpu::ImageDataLayout {
+            offset: 0,
+            bytes_per_row: std::num::NonZeroU32::new((4 * texture.width) as u32),
+            rows_per_image: std::num::NonZeroU32::new(texture.height as u32),
+        },
+        texture_size,
+    );
+    let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
+    let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+        address_mode_u: wgpu::AddressMode::ClampToEdge,
+        address_mode_v: wgpu::AddressMode::ClampToEdge,
+        address_mode_w: wgpu::AddressMode::ClampToEdge,
+        mag_filter: wgpu::FilterMode::Nearest,
+        min_filter: wgpu::FilterMode::Nearest,
+        mipmap_filter: wgpu::FilterMode::Nearest,
+        ..Default::default()
+    });
+    let texture_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStage::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        multisampled: false,
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStage::FRAGMENT,
+                    ty: wgpu::BindingType::Sampler {
+                        comparison: false,
+                        filtering: true,
+                    },
+                    count: None,
+                },
+            ],
+            label: Some("texture_bind_group_layout"),
+        });
+
+    let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &texture_bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+            },
+        ],
+        label: Some("diffuse_bind_group"),
+    });
+    (texture_bind_group_layout, diffuse_bind_group)
+}
+
+fn create_transform_uniform_bind_group(
+    device: &Device,
+    transform: &Transform,
+    camera: (&Camera, &Transform),
+    is_ui_component: bool,
+) -> (GlUniform, Buffer, BindGroupLayout, BindGroup) {
+    let uniform = GlUniform::from(UniformData {
+        transform,
+        camera,
+        is_ui_component,
+    });
+    let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Uniform Buffer"),
+        contents: bytemuck::cast_slice(&[uniform]),
+        usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
+    });
+
+    let uniform_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStage::VERTEX,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+            label: Some("uniform_bind_group_layout"),
+        });
+
+    let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &uniform_bind_group_layout,
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: uniform_buffer.as_entire_binding(),
+        }],
+        label: Some("uniform_bind_group"),
+    });
+
+    (
+        uniform,
+        uniform_buffer,
+        uniform_bind_group_layout,
+        uniform_bind_group,
+    )
+}
+
+fn get_default_color_attachment<'a>(frame: &'a SwapChainTexture, config: &'a ScionConfig,) -> RenderPassColorAttachment<'a> {
+    RenderPassColorAttachment {
+        view: &frame.view,
+        resolve_target: None,
+        ops: wgpu::Operations {
+            load: wgpu::LoadOp::Clear(
+                if let Some(color) = &config.window_config.as_ref().expect("Window config is missing").default_background_color {
+                    wgpu::Color {
+                        r: (color.red() as f32 / 255.) as f64,
+                        g: (color.green() as f32 / 255.) as f64,
+                        b: (color.blue() as f32 / 255.) as f64,
+                        a: color.alpha()  as f64,
+                    }
+                }else{
+                    wgpu::Color {
+                        r: 1.,
+                        g: 0.,
+                        b: 0.,
+                        a: 1.0,
+                    }
+                }),
+            store: true,
+        },
+    }
+}
+
+fn get_no_color_attachment(frame: &SwapChainTexture) -> RenderPassColorAttachment {
+    RenderPassColorAttachment {
+        view: &frame.view,
+        resolve_target: None,
+        ops: wgpu::Operations {
+            load: wgpu::LoadOp::Load,
+            store: true,
+        },
     }
 }
 
