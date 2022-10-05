547306e5b6cf6c38ccd5ecc6fc3bab29861a48e9
Jérémy Thulliez <43185161+grzi@users.noreply.github.com>
2022-10-05 16:36:36 +0200

    feat(taquin): Sort the board at init to be able to play (#246)

--- examples/taquin/main.rs
+++ examples/taquin/main.rs
@@ -1,5 +1,8 @@
 mod animations;
 
+use std::collections::{HashMap, HashSet};
+use rand::rngs::ThreadRng;
+use rand::{Rng, thread_rng};
 use scion::core::world::{GameData, World};
 use scion::{
     config::{scion_config::ScionConfigBuilder, window_config::WindowConfigBuilder},
@@ -32,9 +35,16 @@ struct Taquin {
 }
 
 impl Taquin {
-    fn new() -> Self {
+    fn new(cases: &HashMap<usize, Option<(usize)>>) -> Self {
         let mut board = [[true; 4]; 4];
-        board[3][3] = false;
+        for line in 0..4 {
+            for column in 0..4 {
+                if cases.get(&(line * 4 + column)).unwrap().is_none() {
+                    board[column][line] = false;
+                }
+            }
+        }
+
         Self { board }
     }
 
@@ -65,13 +75,13 @@ fn taquin_system(data: &mut GameData) {
     let mut taquin = resources.get_resource_mut::<Taquin>().unwrap();
 
     let mut animation_running = false;
-    for (e, animations) in world.query_mut::<&mut Animations>() {
-        if animations.any_animation_running(){
+    for (_e, animations) in world.query_mut::<&mut Animations>() {
+        if animations.any_animation_running() {
             animation_running = true;
         }
     }
 
-    if animation_running{
+    if animation_running {
         return;
     }
 
@@ -82,12 +92,10 @@ fn taquin_system(data: &mut GameData) {
                 && mouse_x < (case.0.x() * 192. + 192.) as f64
                 && mouse_y < (case.0.y() * 192. + 192.) as f64
             {
-
                 match taquin.try_move(case.0.x() as usize, case.0.y() as usize) {
                     MoveDirection::Left => {
                         case.0.set_x(case.0.x() - 1.);
                         animations.run_animation("LEFT");
-
                     }
                     MoveDirection::Top => {
                         case.0.set_y(case.0.y() - 1.);
@@ -119,13 +127,17 @@ impl Scene for MainScene {
             4,
             192,
         ));
+
+        let cases = compute_mixed_cases();
+
         for line in 0..4 {
             for column in 0..4 {
-                if !(line == 3 && column == 3) {
+                let case = cases.get(&(line * 4 + column)).expect("Expect all the case to be in the map");
+                if case.is_some() {
                     let square = (
                         Transform::from_xy(column as f32 * 192., line as f32 * 192.),
                         tileset_ref.clone(),
-                        Sprite::new(line * 4 + column),
+                        Sprite::new(case.unwrap()),
                         Case(Coordinates::new(column as f32, line as f32)),
                         Animations::new(get_case_animation())
                     );
@@ -135,8 +147,36 @@ impl Scene for MainScene {
         }
         data.add_default_camera();
 
-        data.insert_resource(Taquin::new());
+        data.insert_resource(Taquin::new(&cases));
     }
+}
+
+fn compute_mixed_cases() -> HashMap<usize, Option<(usize)>> {
+    let mut cases = HashMap::new();
+    // Creating the default board
+    for line in 0..4 {
+        for column in 0..4 {
+            if !(line == 3 && column == 3) {
+                cases.insert(line * 4 + column, Some((line * 4 + column)));
+            } else {
+                cases.insert(line * 4 + column, None);
+            }
+        }
+    }
+
+    // Mixing it with a classic random shuffle algorithm
+    let mut rand = ThreadRng::default();
+    for _i in 0..300 {
+        let a = rand.gen_range(0..cases.len());
+        let b = rand.gen_range(0..cases.len());
+
+        let tmp_a = cases.get(&a).unwrap().clone();
+        let tmp_b = cases.get(&b).unwrap().clone();
+
+        cases.insert(a, tmp_b);
+        cases.insert(b, tmp_a);
+    }
+    cases
 }
 
 fn main() {
@@ -147,7 +187,7 @@ fn main() {
             )
             .get(),
     )
-    .with_system(taquin_system)
-    .with_scene::<MainScene>()
-    .run();
+        .with_system(taquin_system)
+        .with_scene::<MainScene>()
+        .run();
 }
